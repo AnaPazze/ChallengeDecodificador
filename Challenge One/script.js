d99dfd29c377cbdcf8d5ccc061cedb1e19b9fedb
Ana Pazze <112920077+AnaPazze@users.noreply.github.com>
2024-03-06 01:26:01 -0300

    Add files via upload
    Challenge1

---
+++ Challenge One/script.js
@@ -0,0 +1,63 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
+    
+
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e" , "enter"]  ,  ["i" , "imees"]  ,  ["a" , "ai"]  , ["o" , "ober"] , ["u" , "ufat"] ];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++ ) {
+      if(stringEncriptada.includes(matrizCodigo[i][0])) {       
+         stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0] ,  matrizCodigo[i][1]);
+       }
+
+
+    }
+
+    return stringEncriptada;
+
+
+}
+
+function btnDesencriptar() {
+  const textoDesencriptado = desencriptar(textArea.value);
+  mensagem.value = textoDesencriptado;
+}
+
+function desencriptar(stringDesencriptada){
+  let matrizCodigo = [["e" , "enter"]  ,  ["i" , "imees"]  ,  ["a" , "ai"]  , ["o" , "ober"] , ["u" , "ufat"] ];
+  stringDesencriptada = stringDesencriptada.toLowerCase();
+
+  for(let i = 0; i < matrizCodigo.length; i++ ) {
+    if(stringDesencriptada.includes(matrizCodigo[i][1])) {       
+       stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1] ,  matrizCodigo[i][0]);
+     }
+
+
+  }
+
+  return stringDesencriptada;
+
+
+}
+
+  function copiarTexto(){
+    navigator.clipboard.writeText(mensagem.value).then (() =>{
+      console.log('texto copiado para área de tranferência! Texto copiado: ${mensagem.value}');
+    })
+    .catch((error) => {
+      console.error('Falha ao copiar texto para a área de tranferência:', error);
+    });
+
+
+  }
+
+
